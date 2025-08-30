defe5e4d9cc39bb50508a8a910108d2bfb24abc1
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-08-30 08:54:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Tumhara Telegram Bot Token
+BOT_TOKEN = "8357734886:AAHQi1zmj9q8B__7J-2dyYUWVTQrMRr65Dc"
+
+# Tumhara MongoDB URI
+MONGO_URI = "mongodb+srv://afzal99550:afzal99550@cluster0.aqmbh9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DB_NAME = "tempmailbot"
