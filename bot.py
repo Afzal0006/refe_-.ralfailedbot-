6fe66d7d8af8b3f1212a562e6e0a394a649ce495
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-09-15 22:40:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8355581502:AAEawyUncPofLQVzLS_ibLN4WkF8wPa3lVo"
 MONGO_URI = "mongodb+srv://TRUSTLYTRANSACTIONBOT:TRUSTLYTRANSACTIONBOT@cluster0.t60mxb7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 BOT_USERNAME = "Inquiry_chatbot"
 
-OWNER_ID = 6998916494  # <-- Apna Telegram ID yaha daal do
+OWNER_ID = 865032763  # <-- Apna Telegram ID yaha daal do
 
 CHANNELS_URLS = [
     "https://t.me/Trade_With_Gaorav"
